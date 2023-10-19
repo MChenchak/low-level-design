d1db28b97052274c651e9323e87445e1342d6014
Mikhail Chenchak <vtb4055920@region.vtb.ru>
2023-10-19 13:33:28 +0300

    two way list: addd solution

--- src/main/java/two_way_list/TwoWayList.java
+++ src/main/java/two_way_list/TwoWayList.java
@@ -4,6 +4,12 @@ import linked_list.LinkedList;
 
 public abstract class TwoWayList<T> extends ParentList<T>{
 
+    public TwoWayList() {
+    }
+
+    // конструктор
+    public abstract TwoWayList<T> TwoWayList();
+
     public static final int LEFT_OK = 1; // успешно
     public static final int LEFT_ERROR = 0; // левее нет элемента
 
